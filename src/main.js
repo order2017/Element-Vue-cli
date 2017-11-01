4ed5b38b473cbd82fb68bbc018fcaf188716a2e8
陈小龙 <13480731740@qq.com>
2017-11-01 22:05:52 +0800

    引入axios

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI);
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios);
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
